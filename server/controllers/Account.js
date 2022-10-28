46cf64974628e256aa26cdbe838cbb12a6677e2a
kab6379 <kab6379@rit.edu>
2022-10-28 17:29:13 -0400

    Added functionality for DomoMaker B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,4 +1,4 @@
-//const { response } = require('express');
+// const { response } = require('express');
 const models = require('../models');
 
 const { Account } = models;
@@ -7,8 +7,10 @@ const loginPage = (req, res) => res.render('login');
 
 const signupPage = (req, res) => res.render('signup');
 
-const logout = (req, res) => res.redirect('/');
-
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
 const login = (req, res) => {
   const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
@@ -21,6 +23,8 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong username or password!' });
     }
+
+    req.session.account = Account.toAPI(account);
 
     return res.json({ redirect: '/maker' });
   });
@@ -43,6 +47,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) {
     console.log(err);
